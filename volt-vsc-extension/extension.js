f8f7eca79bf4586baf2d467cb9c386e628fffd0b
Andrew Smith <andrew@andrew.codes>
2018-08-06 12:28:06 -0400

    feat(v1) provide command to set an access token

--- volt-vsc-extension/extension.js
+++ volt-vsc-extension/extension.js
@@ -1,30 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const Cache = require("vscode-cache");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
+  const v1Cache = new Cache(context, "versionOne");
+  if (!v1Cache.has("accessToken")) {
+    vscode.window.showInformationMessage(
+      "Please set your VersionOne access token."
+    );
+  }
+  let disposable = vscode.commands.registerCommand(
+    "extension.setAccessToken",
+    function() {
+      vscode.window
+        .showInputBox({
+          password: true,
+          prompt: "Please enter your VersionOne access token"
+        })
+        .then(token => {
+          v1Cache.put("accessToken", token);
+        });
+    }
+  );
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "volt" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
